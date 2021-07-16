3b7c9e7c3b5aa1050054b60a668daaf004c7e5e5
evrinoma <evrinoma@gmail.com>
2021-07-16 11:15:51 +0300

    fix controller

--- src/Controller/HeaderApiController.php
+++ src/Controller/HeaderApiController.php
@@ -3,8 +3,15 @@
 namespace Evrinoma\HeaderBundle\Controller;
 
 use Evrinoma\DtoBundle\Factory\FactoryDtoInterface;
+use Evrinoma\HeaderBundle\Dto\HeaderApiDto;
+use Evrinoma\HeaderBundle\Dto\HeaderApiDtoInterface;
+use Evrinoma\HeaderBundle\Exception\HeaderInvalidException;
+use Evrinoma\HeaderBundle\Exception\HeaderNotFoundException;
 use Evrinoma\UtilsBundle\Controller\AbstractApiController;
+use Evrinoma\UtilsBundle\Rest\RestInterface;
+use FOS\RestBundle\Controller\Annotations as Rest;
 use JMS\Serializer\SerializerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 
@@ -19,6 +26,11 @@ final class HeaderApiController extends AbstractApiController
      * @var FactoryDtoInterface
      */
     private FactoryDtoInterface $factoryDto;
+
+    /**
+     * @var QueryManagerInterface|RestInterface
+     */
+    private QueryManagerInterface $queryManager;
 //endregion Fields
 
 //region SECTION: Constructor
@@ -32,12 +44,74 @@ final class HeaderApiController extends AbstractApiController
     public function __construct(
         SerializerInterface $serializer,
         RequestStack $requestStack,
-        FactoryDtoInterface $factoryDto
+        FactoryDtoInterface $factoryDto,
+        QueryManagerInterface $queryManager,
     ) {
         parent::__construct($serializer);
         $this->request    = $requestStack->getCurrentRequest();
         $this->factoryDto = $factoryDto;
+        $this->queryManager   = $queryManager;
     }
 
+    /**
+     * @Rest\Get("/api/header", options={"expose"=true}, name="api_header")
+     * @OA\Get(
+     *     tags={"header"},
+     *     @OA\Parameter(
+     *         description="class",
+     *         in="query",
+     *         name="class",
+     *         required=true,
+     *         @OA\Schema(
+     *           type="string",
+     *           default="Evrinoma\HeaderBundle\Dto\HeaderApiDto",
+     *           readOnly=true
+     *         )
+     *     ),
+     *      @OA\Parameter(
+     *         description="identity header",
+     *         in="query",
+     *         name="identity",
+     *         required=true,
+     *         @OA\Schema(
+     *           type="string",
+     *           default="app.basic.contr_agent",
+     *         )
+     *     )
+     * )
+     * @OA\Response(response=200,description="Return contractors")
+     *
+     * @return JsonResponse
+     */
+    public function getAction(): JsonResponse
+    {
+        /** @var HeaderApiDtoInterface $contractorApiDto */
+        $headerApiDto = $this->factoryDto->setRequest($this->request)->createDto(HeaderApiDto::class);
+
+        try {
+            $json = $this->queryManager->get($contractorApiDto);
+        } catch (\Exception $e) {
+            $json = $this->setRestStatus($this->queryManager, $e);
+        }
+
+        return $this->setSerializeGroup('api_get_contractor')->json(['message' => 'Get contractors', 'data' => $json], $this->queryManager->getRestStatus());
+    }
+
+
+    private function setRestStatus(RestInterface $manager, \Exception $e): array
+    {
+        switch (true) {
+            case $e instanceof HeaderNotFoundException:
+                $manager->setRestNotFound();
+                break;
+            case $e instanceof HeaderInvalidException:
+                $manager->setRestUnprocessableEntity();
+                break;
+            default:
+                $manager->setRestBadRequest();
+        }
+
+        return ['errors' => $e->getMessage()];
+    }
 //endregion Constructor
 }
