ccf626c9114ab9a7961919e6e062ec2bf0d8c136
Nikolay Nikolaev <evrinoma@gmail.com>
2023-02-03 20:47:29 +0300

    fixed code style v5

--- tests/test.sh
+++ tests/test.sh
@@ -3,10 +3,10 @@
 log=/tmp/test.dump
 echo > $log
 
-rm -rf vendor
-rm -rf composer.lock
-composer install --dev
+#rm -rf vendor
+#rm -rf composer.lock
+#composer install --dev
 rm -rf /tmp/HeaderBundle
 
-/usr/bin/php vendor/phpunit/phpunit/phpunit --bootstrap src/Tests/bootstrap.php --configuration phpunit.xml.dist src/Tests >> $log 2>&1
-echo
+status=$(/usr/bin/php vendor/phpunit/phpunit/phpunit --bootstrap src/Tests/bootstrap.php --configuration phpunit.xml.dist src/Tests >> $log)
+echo $?
