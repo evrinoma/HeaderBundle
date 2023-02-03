ff05e6c3d2ef4a42128a72850af5597cb277b122
Nikolay Nikolaev <evrinoma@gmail.com>
2023-02-03 20:51:06 +0300

    fixed code style v7

--- tests/test.sh
+++ tests/test.sh
@@ -3,10 +3,11 @@
 log=/tmp/test.dump
 echo > $log
 
-#rm -rf vendor
-#rm -rf composer.lock
-#composer install --dev
+rm -rf vendor
+rm -rf composer.lock
+composer install --dev
 rm -rf /tmp/HeaderBundle
 
-/usr/bin/php vendor/phpunit/phpunit/phpunit --bootstrap src/Tests/bootstrap.php --configuration phpunit.xml.dist src/Tests >> $log
-
+/usr/bin/php vendor/phpunit/phpunit/phpunit --bootstrap src/Tests/bootstrap.php --configuration phpunit.xml.dist src/Tests >> $log 2>&1
+status=$(cat /tmp/test.dump | grep "ERRORS!")
+[ $status -eq 0 ] && return 0 ||  exit -1
