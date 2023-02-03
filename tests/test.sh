9ae2134e132cee08da3eba94c693b39628370d0f
Nikolay Nikolaev <evrinoma@gmail.com>
2023-02-03 20:37:26 +0300

    fixed code style

--- tests/test.sh
+++ tests/test.sh
@@ -7,5 +7,6 @@ rm -rf vendor
 rm -rf composer.lock
 composer install --dev
 rm -rf /tmp/HeaderBundle
+
 /usr/bin/php vendor/phpunit/phpunit/phpunit --bootstrap src/Tests/bootstrap.php --configuration phpunit.xml.dist src/Tests >> $log 2>&1
 echo $?
