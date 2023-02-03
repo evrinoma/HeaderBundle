0d9adedcad068a277137b537c204a1ffbe6bbcc8
Nikolay Nikolaev <evrinoma@gmail.com>
2023-02-03 20:55:21 +0300

    fixed code style v9

--- tests/test.sh
+++ tests/test.sh
@@ -10,4 +10,4 @@ rm -rf /tmp/HeaderBundle
 
 /usr/bin/php vendor/phpunit/phpunit/phpunit --bootstrap src/Tests/bootstrap.php --configuration phpunit.xml.dist src/Tests >> $log 2>&1
 status=$(cat /tmp/test.dump | grep "ERRORS!")
-[ -z "$status" ] && return 0 ||  exit -1
+[ -z "$status" ] && exit 0 ||  exit -1
